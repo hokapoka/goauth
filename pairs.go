f856b98db296977102e1c21cc2ac9845e4118de5
Andrew Scott <hoka@hokapoka.com>
2010-08-15 14:36:30 +0100

    Removed old Pairs code

--- pairs.go
+++ pairs.go
@@ -3,56 +3,6 @@ package oauth
 import(
 )
 
-
-/*
-type KeyValuePair struct {
-	Key string
-	Value string
-}
-
-type KeyPairs struct{
-	Items []*KeyValuePair
-}
-
-func (kp KeyPairs) Len() int {  return len(kp.Items)}
-func (kp KeyPairs) Less(i, j int) bool {
-	if kp.Items[i].Key == kp.Items[j].Key {
-		return kp.Items[i].Value < kp.Items[j].Value
-	}
-	return kp.Items[i].Key < kp.Items[j].Key
-}
-func (kp KeyPairs) Swap(i, j int) { kp.Items[i], kp.Items[j] = kp.Items[j], kp.Items[i]}
-
-
-func (kp *KeyPairs) Add( kvp *KeyValuePair){
-
-	if kp.Items == nil { kp.Items = make([]*KeyValuePair, 0, 4) }
-
-	n := len(kp.Items)
-
-	if n+1 > cap(kp.Items) {
-		s := make([]*KeyValuePair, n, 2*n+1)
-		copy(s, kp.Items)
-		kp.Items = s
-	}
-	kp.Items = kp.Items[0 : n+1]
-	kp.Items[n] = kvp
-
-}
-
-func (kp KeyPairs) Get( key string) string { 
-	for i := range kp.Items{
-		if kp.Items[i].Key == key {
-			return  kp.Items[i].Value
-		}
-	}
-	return ""
-}
-
-*/
-
-
-
 type Pair struct {
 	Key string
 	Value string
