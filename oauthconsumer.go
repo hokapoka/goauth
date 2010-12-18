5e275672e2d9f4ecb08fc97f2faa3d2ad4ae33b4
hokapoka <hoka@hokapoka.com>
2010-12-18 14:50:10 +0000

    V 0.0.3
    Fixes to work with Google APIs, replacement of URL Encoder

--- oauthconsumer.go
+++ oauthconsumer.go
@@ -10,7 +10,6 @@ import(
 	"strings"
 	"bytes"
 	"crypto/hmac"
-	"encoding/base64"
 	"io/ioutil"
 	"os"
 )
@@ -25,6 +24,7 @@ type OAuthConsumer struct{
 	ConsumerSecret string
 	CallBackURL string
 	requestTokens []*RequestToken
+	AdditionalParams Params
 }
 
 
@@ -34,28 +34,38 @@ func (oc *OAuthConsumer) GetRequestAuthorizationURL() (string, os.Error){
 	p := Params{}
 
 	// Add required OAuth params
-	p.Add( &Pair{ Key:"oauth_callback", Value:oc.CallBackURL } )
+	p.Add( &Pair{ Key:"oauth_version", Value:"1.0" } )
+	//p.Add( &Pair{ Key:"oauth_nonce", Value:"a0cd80b80b4ea894eedf56491b34d30c" } )
+	p.Add( &Pair{ Key:"oauth_timestamp", Value:strconv.Itoa64(time.Seconds()) } )
 	p.Add( &Pair{ Key:"oauth_consumer_key", Value:oc.ConsumerKey } )
+	p.Add( &Pair{ Key:"oauth_callback", Value:oc.CallBackURL } )
 	p.Add( &Pair{ Key:"oauth_nonce", Value:strconv.Itoa64(rand.Int63()) } )
 	p.Add( &Pair{ Key:"oauth_signature_method", Value:"HMAC-SHA1" } )
-	p.Add( &Pair{ Key:"oauth_timestamp", Value:strconv.Itoa64(time.Seconds()) } )
-	p.Add( &Pair{ Key:"oauth_version", Value:"1.0" } )
 
 	// Sort the collection
 	sort.Sort(p)
 
 	// Generate string of sorted params
-	sigBaseCol := make([]string, len(p))
+	sigBaseCol := make([]string, len(p) + len(oc.AdditionalParams))
 	for i := range p {
-		sigBaseCol[i] = EncodeURL(p[i].Key) + "=" + EncodeURL( p[i].Value )
+		sigBaseCol[i] = Encode(p[i].Key) + "=" + Encode( p[i].Value )
 	}
 
-	sigBaseStr :=	"POST&" +
-					EncodeURL(oc.RequestTokenURL) + "&" +
-					EncodeURL(strings.Join(sigBaseCol, "&"))
+    buf := &bytes.Buffer{}
+
+	i := len(p)
+	for _, kv := range oc.AdditionalParams{
+        buf.Write([]byte(kv.Key + "=" + Encode( kv.Value ) + ""))
+        sigBaseCol[i] = kv.Key + "=" + Encode( kv.Value )
+		i++
+    }
+
+	sigBaseStr :=	"GET&" +
+					Encode(oc.RequestTokenURL) + "&" +
+					Encode(strings.Join(sigBaseCol, "&"))
 
 	// Generate Composite Signing key
-	key := oc.ConsumerSecret + "&" + "" // token secrect is blank on the Resquest Token
+	key := Encode(oc.ConsumerSecret) + "&" + "" // token secrect is blank on the Resquest Token
 
 	// Generate Signature
 	d := oc.digest(key, sigBaseStr)
@@ -63,20 +73,23 @@ func (oc *OAuthConsumer) GetRequestAuthorizationURL() (string, os.Error){
 	// Build Auth Header
 	authHeader := "OAuth "
 	for i := range p {
-		authHeader +=  p[i].Key + "=\"" + EncodeURL(p[i].Value ) + "\", "
+		authHeader +=  p[i].Key + "=\"" + Encode(p[i].Value ) + "\", "
 	}
 
 	// Add the signature
-	authHeader += "oauth_signature=\"" + EncodeURL(d) + "\""
+	authHeader += "oauth_signature=\"" + Encode(d) + "\""
 
-	// Add Header & Buffer for params
-	buf := &bytes.Buffer{}
 	headers := map[string]string{
-		"Content-Type":"application/x-www-form-urlencoded",
+		"Content-Type":"text/plain",
 		"Authorization":authHeader,
 	}
 
-	r, err := post(oc.RequestTokenURL, headers, buf)
+    lAddParams := len(oc.AdditionalParams)
+	if lAddParams > 0 {
+		oc.RequestTokenURL += "?" + string(buf.Bytes())
+	}
+ 
+	r, err := get(oc.RequestTokenURL, headers)
 
 	if err != nil {
 		return "", err
@@ -117,24 +130,29 @@ func (oc *OAuthConsumer) GetAccessToken(token string, verifier string, ) *Access
 
 	// Match the RequestToken by Token
 	for i := range oc.requestTokens {
-		if oc.requestTokens[i].Token == token{
+		if oc.requestTokens[i].Token == Encode(token) {
 			rt = oc.requestTokens[i]
 		}
 	}
 
 	rt.Verifier = verifier
 
+	fmt.Println(rt.Token + " : " + Encode(rt.Token))
+	fmt.Println(rt.Verifier + " : " + Encode(rt.Verifier))
+	fmt.Println(rt.Secret + " : " + Encode(rt.Secret))
+
+
 	// Gather the params
 	p := Params{}
 
 	// Add required OAuth params
 	p.Add( &Pair{ Key:"oauth_consumer_key", Value:oc.ConsumerKey } )
-	p.Add( &Pair{ Key:"oauth_nonce", Value:strconv.Itoa64(rand.Int63()) } )
-	p.Add( &Pair{ Key:"oauth_signature_method", Value:"HMAC-SHA1" } )
-	p.Add( &Pair{ Key:"oauth_timestamp", Value:strconv.Itoa64(time.Seconds()) } )
-	p.Add( &Pair{ Key:"oauth_version", Value:"1.0" } )
 	p.Add( &Pair{ Key:"oauth_token", Value:rt.Token })
 	p.Add( &Pair{ Key:"oauth_verifier", Value:rt.Verifier })
+	p.Add( &Pair{ Key:"oauth_signature_method", Value:"HMAC-SHA1" } )
+	p.Add( &Pair{ Key:"oauth_timestamp", Value:strconv.Itoa64(time.Seconds()) } )
+	p.Add( &Pair{ Key:"oauth_nonce", Value:strconv.Itoa64(rand.Int63()) } )
+	p.Add( &Pair{ Key:"oauth_version", Value:"1.0" } )
 
 	// Sort the collection
 	sort.Sort(p)
@@ -142,15 +160,19 @@ func (oc *OAuthConsumer) GetAccessToken(token string, verifier string, ) *Access
 	// Generate string of sorted params
 	sigBaseCol := make([]string, len(p))
 	for i := range p {
-		sigBaseCol[i] = EncodeURL(p[i].Key) + "=" + EncodeURL( p[i].Value )
+		sigBaseCol[i] = Encode(p[i].Key) + "=" + Encode( p[i].Value )
 	}
 
 	sigBaseStr :=	"POST&" +
-					EncodeURL(oc.AccessTokenURL) + "&" +
-					EncodeURL(strings.Join(sigBaseCol, "&"))
+					Encode(oc.AccessTokenURL) + "&" +
+					Encode(strings.Join(sigBaseCol, "&"))
+
+	sigBaseStr= strings.Replace(sigBaseStr, Encode(Encode(rt.Token)), Encode(rt.Token), 1)
+
+	fmt.Println(sigBaseStr);
 
 	// Generate Composite Signing key
-	key := oc.ConsumerSecret + "&" + rt.Secret
+	key := Encode(oc.ConsumerSecret) + "&" + rt.Secret
 
 	// Generate Signature
 	d := oc.digest(key, sigBaseStr)
@@ -158,11 +180,14 @@ func (oc *OAuthConsumer) GetAccessToken(token string, verifier string, ) *Access
 	// Build Auth Header
 	authHeader := "OAuth "
 	for i := range p {
-		authHeader +=  p[i].Key + "=\"" + EncodeURL(p[i].Value ) + "\", "
+		authHeader +=  p[i].Key + "=\"" + Encode(p[i].Value ) + "\", "
 	}
 
+
 	// Add the signature
-	authHeader += "oauth_signature=\"" + EncodeURL(d) + "\""
+	authHeader += "oauth_signature=\"" + Encode(d) + "\""
+
+	authHeader = strings.Replace(authHeader, Encode(rt.Token), rt.Token, 1)
 
 	// Add Header & Buffer for params
 	buf := &bytes.Buffer{}
@@ -171,8 +196,17 @@ func (oc *OAuthConsumer) GetAccessToken(token string, verifier string, ) *Access
 		"Authorization":authHeader,
 	}
 
+/*	fmt.Println(" ** URL ** ")
+	fmt.Println(oc.AccessTokenURL)
+	fmt.Println("** Auth Header **" )
+	fmt.Println(authHeader)
+
+	fmt.Println("** SIG  **" )
+	fmt.Println(d)
+*/
 	// Action the POST to get the AccessToken
 	r, err :=  post(oc.AccessTokenURL, headers, buf)
+
 	if err != nil {
 		fmt.Println(err.String())
 		return nil
@@ -182,6 +216,8 @@ func (oc *OAuthConsumer) GetAccessToken(token string, verifier string, ) *Access
 	b, _ := ioutil.ReadAll( r.Body ) 
 	s := string(b)
 	at := &AccessToken{}
+
+	fmt.Println(s)
 
 	if strings.Index(s, "&") > -1 {
 		vals := strings.Split(s, "&", 10)
@@ -205,8 +241,8 @@ func (oc *OAuthConsumer) GetAccessToken(token string, verifier string, ) *Access
 }
 
 // OAuthRequestGet return the response via a GET for the url with the AccessToken passed
-func (oc *OAuthConsumer) Get( url string, at *AccessToken) (r *http.Response, err os.Error) {
-	return oc.oAuthRequest(url, Params{}, at, "GET")
+func (oc *OAuthConsumer) Get( url string, fparams Params, at *AccessToken) (r *http.Response, err os.Error) {
+	return oc.oAuthRequest(url, fparams, at, "GET")
 }
 
 // OAuthRequest returns the response via a POST for the url with the AccessToken passed & the Form params passsed in fparams
@@ -217,21 +253,23 @@ func (oc *OAuthConsumer) Post( url string, fparams Params, at *AccessToken) (r *
 func (oc *OAuthConsumer) oAuthRequest( url string, fparams Params, at *AccessToken, method string) (r *http.Response, err os.Error) {
 
 	fmt.Println("***************************** DO REQUEST **********************")
+
 	// Gather the params
 	p := Params{}
 
+	hp := Params{}
+
 	// Add required OAuth params
-	//p.Add( &Pair{ Key:"oauth_callback", Value:oc.CallBackURL } )
-	p.Add( &Pair{ Key:"oauth_consumer_key", Value:oc.ConsumerKey } )
-	p.Add( &Pair{ Key:"oauth_nonce", Value:strconv.Itoa64(rand.Int63()) } )
+	p.Add( &Pair{ Key:"oauth_token", Value:at.Token })
 	p.Add( &Pair{ Key:"oauth_signature_method", Value:"HMAC-SHA1" } )
+	p.Add( &Pair{ Key:"oauth_consumer_key", Value:oc.ConsumerKey } )
 	p.Add( &Pair{ Key:"oauth_timestamp", Value:strconv.Itoa64(time.Seconds()) } )
+	p.Add( &Pair{ Key:"oauth_nonce", Value:strconv.Itoa64(rand.Int63()) } )
 	p.Add( &Pair{ Key:"oauth_version", Value:"1.0" } )
 
-
-	// Add the token params if passed
-	if at != nil {
-		p.Add( &Pair{ Key:"oauth_token", Value:at.Token })
+	// Add the params to the Header collection
+	for i := range p {
+		hp.Add( &Pair{ Key:p[i].Key, Value:p[i].Value } )
 	}
 
 	fparamsStr := ""
@@ -239,7 +277,7 @@ func (oc *OAuthConsumer) oAuthRequest( url string, fparams Params, at *AccessTok
 	for i := range fparams{
 		k, v := fparams[i].Key, fparams[i].Value
 		p.Add( &Pair{ Key:k, Value:v } )
-		fparamsStr += k + "=" + v + "&"
+		fparamsStr += k + "=" + Encode(v) + "&"
 	}
 
 	// Sort the collection
@@ -248,42 +286,59 @@ func (oc *OAuthConsumer) oAuthRequest( url string, fparams Params, at *AccessTok
 	// Generate string of sorted params
 	sigBaseCol := make([]string, len(p))
 	for i := range p {
-		sigBaseCol[i] = EncodeURL(p[i].Key) + "=" + EncodeURL( p[i].Value )
+		sigBaseCol[i] = Encode(p[i].Key) + "=" + Encode( p[i].Value )
 	}
 
 	sigBaseStr :=	method + "&" +
-					EncodeURL(url) + "&" +
-					EncodeURL(strings.Join(sigBaseCol, "&"))
+					Encode(url) + "&" +
+					Encode(strings.Join(sigBaseCol, "&"))
+
+	sigBaseStr= strings.Replace(sigBaseStr, Encode(Encode(at.Token)), Encode(at.Token), 1)
 
 	// Generate Composite Signing key
-	key := oc.ConsumerSecret + "&" + at.Secret
+	key := Encode( oc.ConsumerSecret ) + "&" + at.Secret 
 
 	// Generate Signature
 	d := oc.digest(key, sigBaseStr)
 
 	// Build Auth Header
 	authHeader := "OAuth "
-	for i := range p {
-		if strings.Index(p[i].Key, "oauth") == 0 {
+	for i := range hp {
+		if strings.Index(hp[i].Key, "oauth") == 0 {
 			//Add it to the authHeader
-			authHeader +=  p[i].Key + "=\"" + EncodeURL(p[i].Value ) + "\", "
+			authHeader += hp[i].Key + "=\"" + Encode(hp[i].Value ) + "\", "
 		}
 	}
 
 	// Add the signature
-	authHeader += "oauth_signature=\"" + EncodeURL(d) + "\""
+	authHeader += "oauth_signature=\"" + Encode(d) + "\""
+
+	authHeader = strings.Replace(authHeader, Encode(at.Token), at.Token, 1)
+
+	fmt.Println(at.Token)
 
 	// Add Header & Buffer for params
 	buf := bytes.NewBufferString(fparamsStr)
 	headers := map[string]string{
-		"Content-Type":"application/x-www-form-urlencoded",
+//		"Content-Type":"application/atom+xml",
 		"Authorization":authHeader,
 	}
 
+	fmt.Println(" ** URL ** ")
+	fmt.Println(url)
+	fmt.Println("** Auth Header **" )
 	fmt.Println(authHeader)
+
+	fmt.Println("** SIG Base **" )
+	fmt.Println(sigBaseStr)
+
+
+	fmt.Println("** SIG  **" )
+	fmt.Println(d)
+
 	if method == "GET" {
 		// return Get response
-		return get(url, headers)
+		return get(url + "?" + fparamsStr, headers)
 	}
 
 	// return POSTs response
@@ -296,11 +351,15 @@ func (oc *OAuthConsumer) oAuthRequest( url string, fparams Params, at *AccessTok
 func (oc *OAuthConsumer) digest(key string, m string) string {
 	h := hmac.NewSHA1([]byte(key))
 	h.Write([]byte(m))
-	s := bytes.TrimSpace(h.Sum())
+	return base64encode(h.Sum())
+
+/*	s := bytes.TrimSpace(h.Sum())
 	d := make([]byte, base64.StdEncoding.EncodedLen(len(s)))
 	base64.StdEncoding.Encode(d, s)
 	ds := strings.TrimSpace(bytes.NewBuffer(d).String())
-	return ds
+*/
+//	return ds
+
 }
 
 // appendRequestToken adds the Request Tokens to a localy temp collection
--- urlencode.go
+++ urlencode.go
@@ -1,27 +1,51 @@
+// Copyright 2010 Calvin McAnarney <calvin@mcanarney.org>
+//
+// Use of this file is governed by the ISC license. See the LICENSE file for
+// details.
+
 package oauth
 
 import (
 	"bytes"
-	"utf8"
-	"fmt"
+	"encoding/base64"
 )
 
-var safe = []byte("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz._-")
-
-func EncodeURL(u string) string {
-	var b bytes.Buffer
-	for _, c := range u {
-		b.Write(encodeChar(c))
-	}
-	return b.String()
+// base64encode returns a string representation of Base64-encoded src.
+func base64encode(src []byte) string {
+	var buf bytes.Buffer
+	encoder := base64.NewEncoder(base64.StdEncoding, &buf)
+	encoder.Write(src)
+	encoder.Close()
+	return buf.String()
 }
 
-func encodeChar(c int) []byte {
-	b := make([]byte, utf8.RuneLen(c))
-	utf8.EncodeRune(b, c)
-	if bytes.Index(safe, b) != -1 {
-		return b
+// Encode percent-encodes a string as defined in RFC 3986.
+func Encode(s string) string {
+	var enc string
+	for _, c := range []byte(s) {
+		if isEncodable(c) {
+			enc += "%"
+			enc += string("0123456789ABCDEF"[c>>4])
+			enc += string("0123456789ABCDEF"[c&15])
+		} else {
+			enc += string(c)
+		}
 	}
-	return []byte(fmt.Sprintf("%%%02X", c))
+	return enc
+}
+
+// isEncodable returns true if a given character should be percent-encoded
+// according to RFC 3986.
+func isEncodable(c byte) bool {
+	// return false if c is an unreserved character (see RFC 3986 section 2.3)
+	switch {
+	case (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'):
+		return false
+	case c >= '0' && c <= '9':
+		return false
+	case c == '-' || c == '.' || c == '_' || c == '~':
+		return false
+	}
+	return true
 }
 
